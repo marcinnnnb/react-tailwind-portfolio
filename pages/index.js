9fcc205d235dc0be1ce32db322eaf188dd499a2e
Marcin Barszcz <marcin.barszcz@op.pl>
2023-01-23 23:19:09 +0100

    feature: about me section

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,20 @@ export default function Home() {
             <Image src={deved} layout="fill" objectFit="cover" alt="Image of Ed" />
           </div>
         </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the begining of my journey as a freelance design and developer, I&apos;ve done remote work for
+              <span className="text-teal-500"> agencies </span>consulted for
+              <span className="text-teal-500"> startups </span> and collaborated with talented people to create digital
+              products for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including brand design, programming and teaching.
+            </p>
+          </div>
+        </section>
       </main>
     </div>
   );
